8c850daa9858a270e137d894e233a9709e8f806b
o9u <1234@fexpost.com>
2024-10-30 08:28:02 +0300

    ref/zod simplified

--- src/hooks/useSetDataToLS.ts
+++ src/hooks/useSetDataToLS.ts
@@ -1,46 +1,15 @@
 import React from 'react';
 import { TItem } from '../pages/home-page';
-import { TCartItem } from '../store';
 
 export const useSetDataToLS = ({
-  // items,
-  // page,
-  // limit,
   dataAll,
-  // param,
   btnState,
   id,
 }: {
-  items?: TCartItem[];
-  page?: number;
-  limit?: number;
   dataAll?: TItem[];
-  param?: string;
   btnState?: boolean;
   id?: string;
 }) => {
-  // const isMounted = React.useRef(false);
-  // {
-  //   React.useEffect(() => {
-  //     if (isMounted.current && items) {
-  //       localStorage.setItem('data', JSON.stringify(items));
-  //     }
-  //     isMounted.current = true;
-  //   }, [items]);
-  // }
-
-  // {
-  //   React.useEffect(() => {
-  //     if (page) localStorage.setItem('pagPage', page.toString());
-  //   }, [page]);
-  // }
-
-  // {
-  //   React.useEffect(() => {
-  //     if (limit) localStorage.setItem('pagLimit', limit.toString());
-  //   }, [limit]);
-  // }
-
   {
     React.useEffect(() => {
       if (dataAll && dataAll.length > 0) {
@@ -51,11 +20,6 @@ export const useSetDataToLS = ({
       }
     }, [dataAll]);
   }
-  // {
-  //   React.useEffect(() => {
-  //     if (param) localStorage.setItem('searchParam', param);
-  //   }, [param]);
-  // }
   {
     React.useEffect(() => {
       if (id && btnState) {
--- src/utils/getDataFromLS.ts
+++ src/utils/getDataFromLS.ts
@@ -1,58 +1,22 @@
-import {
-  // cartItemsSchema,
-  colorsSchema,
-  // limitSchema,
-  // pageSchema,
-  // parameterSchema,
-  stateSchema,
-} from './validationLS';
+import { z } from 'zod';
 import { parseData, resetData } from './preparingData';
-// import { parseData, resetData } from '.';
-// import { DEFAULT_ITEM_COUNT, DEFAULT_LIMIT, DEFAULT_PARAM } from './constants';
+
+const colorsSchema = z.array(z.object({ id: z.string(), color: z.string() }));
+const stateSchema = z.coerce.boolean();
 
 export const getDataFromLS = (id?: string) => {
-  //getting data from ls for items/page/limit/param/colors/state
-  // const storedItems = parseData('data');
   const storedCol = parseData('data-colors');
-
-  // const storedPage = localStorage.getItem('pagPage');
-  // const storedLim = localStorage.getItem('pagLimit') || DEFAULT_LIMIT;
-  // const storedParam = localStorage.getItem('searchParam');
   const storedSt = localStorage.getItem(`btnState-${id}`);
 
-  //count items and pages
-  // const itemCount = storedCol ? storedCol.length : DEFAULT_ITEM_COUNT;
-  // const pageCount = storedLim ? Math.ceil(itemCount / +storedLim) : itemCount;
-  //validate items
-  // const parsedItems = cartItemsSchema.safeParse(storedItems);
-  // const validatedItems = parsedItems.success ? parsedItems.data : [];
-  // if (!parsedItems.success) resetData('data');
-  //validate colors
   const parsedCol = colorsSchema.safeParse(storedCol);
   const validatedCol = parsedCol.success ? parsedCol.data : [];
   if (!parsedCol.success) resetData('data-colors');
-  //validate limit
-  // const parsedLim = limitSchema(itemCount).safeParse(storedLim);
-  // const validatedLim = parsedLim.success ? parsedLim.data : DEFAULT_LIMIT;
-  //validate param
-  // const parsedParam = parameterSchema.safeParse(storedParam);
-  // const validatedParam = parsedParam.success ? parsedParam.data : DEFAULT_PARAM;
-  //validate page
-  // const parsedPage = pageSchema(pageCount).safeParse(storedPage);
-  // const validatedPage = parsedPage.success ? parsedPage.data : 1;
-  //validate state
+
   const parsedSt = stateSchema.safeParse(storedSt);
   const validatedSt = parsedSt.success ? parsedSt.data : false;
 
   return {
-    // items: validatedItems,
-    // initLim: validatedLim,
-    // initParam: validatedParam,
-    // initPage: validatedPage,
-    // initAct: validatedPage,
     initCol: validatedCol,
     initSt: validatedSt,
-    // total,
-    // totalQnt,
   };
 };
